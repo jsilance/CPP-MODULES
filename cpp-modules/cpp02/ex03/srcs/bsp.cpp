dedf7fcf92db749c8ad8c460059484a99bd37862
Julien Silance <jusilanc@fu-c3-p3.s19.be>
2023-08-20 15:45:22 +0200

    fix cpp02

--- cpp-modules/cpp02/ex03/srcs/bsp.cpp
+++ cpp-modules/cpp02/ex03/srcs/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 18:37:54 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/07/30 18:58:36 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/08/20 15:44:46 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed alpha = ((b.getY() - c.getY())*(point.getX() - c.getX()) + (c.getX() - b.getX())*(point.getY() - c.getY())) / ((b.getY() - c.getY())*(a.getX() - c.getX()) + (c.getX() - b.getX())*(a.getY() - c.getY()));
-    Fixed beta  = ((c.getY() - a.getY())*(point.getX() - c.getX()) + (a.getX() - c.getX())*(point.getY() - c.getY())) / ((b.getY() - c.getY())*(a.getX() - c.getX()) + (c.getX() - b.getX())*(a.getY() - c.getY()));
+	Fixed alpha = ((b.getY() - c.getY()) * (point.getX() - c.getX()) + (c.getX() - b.getX()) * (point.getY() - c.getY())) / ((b.getY() - c.getY()) * (a.getX() - c.getX()) + (c.getX() - b.getX()) * (a.getY() - c.getY()));
+    Fixed beta  = ((c.getY() - a.getY()) * (point.getX() - c.getX()) + (a.getX() - c.getX()) * (point.getY() - c.getY())) / ((b.getY() - c.getY()) * (a.getX() - c.getX()) + (c.getX() - b.getX()) * (a.getY() - c.getY()));
     Fixed gamma = Fixed(1) - alpha - beta;
 
     return (alpha > 0 && beta > 0 && gamma > 0 && (alpha + beta + gamma) == 1);
