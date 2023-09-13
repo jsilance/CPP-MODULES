65f7b184a1a5aea4bd4a34c574a667f27607ec18
juliensilance@hotmail.com <juliensilance@mbp-de-julien.home>
2023-09-14 00:24:11 +0200

    maj

--- cpp05/ex02/srcs/class/AForm.hpp
+++ cpp05/ex02/srcs/class/AForm.hpp
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 16:07:45 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/09/13 13:21:51 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/09/13 23:55:47 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ class AForm
 	public:
 		AForm();
 		AForm(std::string name, int gToSign, int gToEx);
-		~AForm();
+		virtual ~AForm();
 
 		AForm &operator=(AForm const & src);
 
