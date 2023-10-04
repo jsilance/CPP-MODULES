cf1e4eab67843db06bb4cb5e83c1d872e76595de
jsilance <juliensilance@hotmail.com>
2023-10-04 16:24:33 +0200

    finished cpp07

--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 01:24:55 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/10/03 16:47:12 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/10/04 16:23:52 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,31 @@ int main()
 			std::cout << tabInt[i] << " ";
 		std::cout << std::endl << "Array size: " << tabInt.size() << std::endl;
 		std::cout << "Test of Out of Range...\n" << tabInt[tabInt.size() + 3] << std::endl;
-
-
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
 	
+	try
+	{
+		Array<std::string> words(6);
+		words[0] = "Hello world";
+		words[1] = "Everything is fine";
+		words[2] = "Can I have a HOO YEAH!?";
+		words[3] = "I'm pickle Rick";
+		words[4] = "Tu sais pas qui je suis";
+		words[5] = "J'ai des connaissance en chimie...";
+
+		for (int i = 0; i < words.size(); i++)
+			std::cout << words[i] << "\n";
+		std::cout << std::endl << "Array size: " << words.size() << std::endl;
+		words[6] = "I'm a out of range operation lel";
+		std::cout << "Test of Out of Range...\n" << words[words.size() + 3] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+	}
 	return (0);
 }
