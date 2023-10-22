cec656108349b8b8c711282216ff445e5f48d54f
Julien Silance <jusilanc@fu-c3-p5.s19.be>
2023-10-22 20:30:32 +0200

    on the way

--- cpp09/ex02/srcs/class/PmergeMe.cpp
+++ cpp09/ex02/srcs/class/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 12:42:15 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/10/21 12:46:30 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/10/22 20:19:46 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,52 @@ PmergeMe::~PmergeMe()
 
 PmergeMe & PmergeMe::operator=(const PmergeMe & src)
 {
+	if (this != &src)
+	{
+	}
+	return (*this);
+}
+
+template<typename T> void PmergeMe::diplayNumber(const T & container)
+{
+	for (typename T::const_iterator it = container.begin(); it != container.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+}
+
+template<typename T> void PmergeMe::_merge(T & container)
+{
+	typename T::iterator it = container.begin();
+	
+	container.size();
+}
+
+template<typename T> void PmergeMe::_binaryInsertionSort(T begin, T end, int nbr)
+{
+	T middle = begin + std::distance(begin, end) / 2;
+
+	if (std::distance(begin, end) == 1 || *middle == nbr)
+	{
+		if (*middle >= nbr)
+		{
+			if (dynamic_cast<std::deque<int>::iterator> (begin))	
+				this->_deq.insert(middle, nbr);
+			else if (dynamic_cast<std::vector<int>::iterator> (begin))
+				this->_vec.insert(middle, nbr);
+			else
+				std::cout << "Error: container not supported" << std::endl;
+		}
+		else
+			end = container.insert(end, nbr);
+		return ;
+	}
+	if (nbr < *middle)
+		_binaryInsertionSort(begin, middle, nbr);
+	else if (nbr > *middle)
+		_binaryInsertionSort(middle, end, nbr);
+}
+
+template<typename T> void PmergeMe::mergeInsertSort(T & deq)
+{
+	this->merge();
 }
--- cpp09/ex02/srcs/class/PmergeMe.hpp
+++ cpp09/ex02/srcs/class/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 12:37:14 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/10/21 12:53:21 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/10/22 19:26:37 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,27 @@
 # define PMERGEME_HPP
 
 # include <deque>
+# include <vector>
 # include <list>
 
 class PmergeMe
 {
 	private:
+
+		std::deque<int>	_deq;
+		std::vector<int> _vec;
+	
 		PmergeMe();
 		PmergeMe(const PmergeMe & src);
 
 		PmergeMe & operator=(const PmergeMe & src);
+		template<typename T> void _merge(T begin, T, T end);
+		template<typename T> void _binaryInsertionSort(T begin, T end, int nbr);
 	public:
 		~PmergeMe();
 
 		template<typename T> void diplayNumber(const T & container);
-		void mergeInsetDeque(std::deque<int> & deq);
-		void mergeInsetList(std::list<int> & lst);
-
+		template<typename T> void mergeInsertSort(T & container);
 };
 
 #endif
